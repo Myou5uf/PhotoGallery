17ef95b70ad2dbe62ba00c1df1c44f52e5382775
Myou5uf <myxamed12l@gmail.com>
2020-12-23 22:41:23 +0700

    10. Добавил в приложение возможность поиска фотографий на Flickr. Добавил выпадающее меню на панель инструментов

--- PhotoGallery/app/src/main/java/com/example/a/photogallerry/PhotoGallery.java
+++ PhotoGallery/app/src/main/java/com/example/a/photogallerry/PhotoGallery.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
+import android.app.SearchManager;
+import android.content.Context;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.example.a.photogallerry.api.FlickrAPI;
 import com.example.a.photogallerry.api.ServiceAPI;
 import com.example.a.photogallerry.model.Photo;
 import com.example.a.photogallerry.model.PhotosResponse;
+import com.example.a.photogallerry.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,9 +58,71 @@ public class PhotoGallery extends AppCompatActivity {
             }
         });
         recyclerView.setAdapter(photoAdapter);
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.item_menuSearch).getActionView();
+
+        searchView.setOnQueryTextListener(onQueryTextListener);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        return true;
+    }
+    protected SearchView.OnQueryTextListener onQueryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            flickrAPI.getSearchPhotos(query).enqueue(new Callback<PhotosResponse>() {
+
+                @Override
+                public void onResponse(Call<PhotosResponse> call, Response<PhotosResponse> response) {
+                    list_photo.clear();
+                    list_photo.addAll(response.body().getPhotos().getPhoto());
+                    photoAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onFailure(Call<PhotosResponse> call, Throwable t) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(PhotoGallery.this).create(); //Read Update
+                    alertDialog.setTitle("Info");
+                    alertDialog.setMessage("Something went wrong!");
+                    alertDialog.show();
+                }
+            });
+            return true;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            return true;
+        }
+    };
+
+    public void onLoadRecentClick(MenuItem item) {
+        flickrAPI.getRecent().enqueue(new Callback<PhotosResponse>() {
+
+            @Override
+            public void onResponse(Call<PhotosResponse> call, Response<PhotosResponse> response) {
+                list_photo.clear();
+                list_photo.addAll(response.body().getPhotos().getPhoto());
+                photoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<PhotosResponse> call, Throwable t) {
+                AlertDialog alertDialog = new AlertDialog.Builder(PhotoGallery.this).create(); //Read Update
+                alertDialog.setTitle("Info");
+                alertDialog.setMessage("Something went wrong!");
+                alertDialog.show();
+            }
+        });
+    }
+
+    public void onLoadGalleryClick(MenuItem item) {
+
+    }
 }
